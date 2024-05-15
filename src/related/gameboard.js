9674b9df58292087c3d9f88ecc7babfeda196c0a
Orbter <assouline39@gmail.com>
2024-05-15 17:15:14 +0300

    created an creating a matrix array

--- src/related/gameboard.js
+++ src/related/gameboard.js
@@ -1,1 +1,13 @@
 import { Ship } from './ship';
+
+function createMatrix(rows, cols) {
+  const matrix = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      row.push(0); // You can initialize the matrix with any value, here we use 0
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
